67f573d065e893d21a1cf4b282dd1f55472d8edf
Jhon Bohorquez <firsoftsi@gmail.com>
2024-07-11 16:53:23 -0500

    Agregar archivos de proyecto.

---
+++ ApiHoteles/ScriptsBD/ScriptDML.sql
@@ -0,0 +1,20 @@
+﻿USE [Hospedaje]
+GO
+
+INSERT INTO [dbo].[Hotel]
+           ([NameHotel]
+           ,[Categoria]
+           ,[Precio]
+           ,[General]
+           ,[Habitacion]
+           ,[Cama]
+           ,[Calificacion])
+     VALUES
+           (<NameHotel, varchar(50),>
+           ,<Categoria, char(1),>
+           ,<Precio, numeric(18,0),>
+           ,<General, varchar(max),>
+           ,<Habitacion, varchar(max),>
+           ,<Cama, varchar(max),>
+           ,<Calificacion, varchar(50),>)
+GO
